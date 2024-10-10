2074e6159e089771839bc0b11965c38b2d8457c4
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-10-10 06:45:19 +0000

    test doctype

---
+++ my_app01/my_app01_test/doctype/my_test_doctype/test_my_test_doctype.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2024, ztest95 and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class TestMyTestDocType(UnitTestCase):
+	"""
+	Unit tests for MyTestDocType.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
+
+
+class TestMyTestDocType(IntegrationTestCase):
+	"""
+	Integration tests for MyTestDocType.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
